ee5b692fdc3224542bcced57231716e8f9ed4ede
Nick Fisher <github@nickdafish.com>
2013-07-07 18:38:31 -0400

    cfg and OO

--- src/air_codea.py
+++ src/air_codea.py
@@ -2,16 +2,37 @@
 import requests as req
 from lxml import html
 import json
+from ConfigParser import ConfigParser
 
-ip = '192.168.1.6'
-port = '42000'
-project = 'Synkit'
-base = 'http://%s:%s/projects/%s' % (ip, port, project)
 
-main = open('Main.lua').read()
+class CodeaProject(object):
+    def __init__(self, ip, port, project):
+        self.base = 'http://%s:%s/projects/%s' % (ip, port, project)
+        # Always make sure we are on the right project
+        self._check_files()
 
-req.get(base)
+    def _check_files(self):
+        # FIXME error checking
+        resp = req.get(self.base)
+        xpath = '//div[@class="project-title"]'
+        files = [e.text for e in html.fromstring(resp.content).xpath(xpath)]
+        self.files = files
 
-d = json.dumps({'file':'Main', 'contents':main})
-print d
-u = req.post(base+'/__update', data=d)
+    def upload_file(self, filename):
+        text = open(filename).read()
+        resp = req.post(self.base+'/__update',
+            data=json.dumps({
+                'file':'Main', 
+                'contents':text
+            })
+        )
+
+cfg = ConfigParser()
+cfg.read('.air_codea.cfg')
+# I can do **dict(cfg.items('connection')) or similar... but what errors?
+ip = cfg.get('connection', 'ip')
+port = cfg.get('connection', 'port')
+project = cfg.get('connection', 'project')
+
+cp = CodeaProject(ip, port, project)
+cp.upload_file('Main.lua')
