ad738d521ff16fb01629abc426b663f6a5e442a0
Nick Fisher <github@nickdafish.com>
2014-05-03 14:48:03 -0400

    converting to phantomjs

--- src/air_codea.py
+++ src/air_codea.py
@@ -3,46 +3,61 @@ from lxml import html
 import json
 from hashlib import md5
 import logging
+from splinter import Browser
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
+from time import sleep
+
+SLEEP_TIME = 0.5
 
 log = logging.getLogger('base')
-
 
 class CodeaProject(object):
     def __init__(self, ip, port, project):
         self.base = 'http://%s:%s/projects/%s' % (ip, port, project)
+        # FIXME set a low timeout or somehow test the base string first
+        self.browser = Browser('phantomjs')
+
         # Always make sure we are on the right project
         self._check_files()
 
     def _check_files(self):
         log.info('Checking files @ %s' % self.base)
-        resp = req.get(self.base)
-        if resp.status_code!= 200:
-            log.debug(resp.content)
+        self.browser.visit(self.base)
+        # FIXME check return code
         xp = '//ul[@class="tabs"]/a[@href!="/"]/li'
-        files = [e.text for e in html.fromstring(resp.content).xpath(xp)]
+        files = [e.text for e in self.browser.find_by_xpath(xp)]
         self.files = files
         log.info('Found %s' % ', '.join(files))
 
     def upload_file(self, filename):
         log.info('Uploading %s' % filename)
         text = open(filename + '.lua').read()
-        resp = req.post(self.base + '/__update',
-            data=json.dumps({
-                'file': filename,
-                'contents': text
-            })
-        )
-        if resp.status_code!= 200:
-            log.debug(resp.content)
+        self.browser.visit('%s/%s' % (self.base, filename))
+        # FIXME check return code
+        js = "editor.setValue('%s')" % repr(text)[1:-1]
+        self.browser.execute_script(js)
+
+        # Wait to for the ACE editor to catch up
+        sleep(SLEEP_TIME)
+
+        try:
+            js = 'editor.getSession().getAnnotations()[0].text'
+            ret = self.browser.evaluate_script(js)
+        except WebDriverException, e:
+            # No errors detected
+            pass
+        else:
+            # FIXME this should be logged
+            print filename, ':', ret
+            
         return md5(text).hexdigest()
 
     def get_file(self, filename):
         log.info('Getting %s' % filename)
-        resp = req.get('%s/%s' % (self.base, filename))
-        if resp.status_code!= 200:
-            log.debug(resp.content)
-        xp = '//div[@id="editor"]'
-        text = html.fromstring(resp.content).xpath(xp)[0].text
+        self.browser.visit('%s/%s' % (self.base, filename))
+        # FIXME check return code
+        text = self.browser.evaluate_script('editor.getSession().getValue()')
         if text is None:  # Blank file
             log.info('Got blank file')
             text = ''
@@ -55,6 +70,4 @@ class CodeaProject(object):
 
     def restart(self):
         log.info('Restarting')
-        resp = req.get(self.base + '/__restart')
-        if resp.status_code!= 200:
-            log.debug(resp.content)
+        self.browser.find_by_css('.ace_text-input')[0].type(Keys.CONTROL+'r')
